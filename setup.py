bafc2896c2a3ba903b1064c1c5c65ef3615f0231
Sangho Na <microamp@pm.me>
2021-05-06 11:29:10 +1200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="aws-glue-libs",
+    version="0.0.1",
+    long_description=__doc__,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
